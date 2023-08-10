6fa4ffad3e7406e7933f0f2540bfb08e93532497
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-08-10 23:58:33 +0300

    1213

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const productsController = require('./products');
 const dailyController = require('./daily');
 
 const test = () => {
-    console.log("test");
+    console.log("test pagegit ");
 }
 
 module.exports = { authController, productsController, dailyController };
