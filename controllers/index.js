bc949609974c2f11f94c7f597ddff81157c9dfda
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-08-10 16:23:36 +0300

    commit 2

--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,8 @@ const authController = require('./auth');
 const productsController = require('./products');
 const dailyController = require('./daily');
 
+const test = () => {
+    
+}
+
 module.exports = { authController, productsController, dailyController };
