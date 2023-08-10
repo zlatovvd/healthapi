f15bfda748fc5b8c9579b5c3dc2f386a9ba5e11d
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-08-10 23:07:57 +0300

    update h-page

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const productsController = require('./products');
 const dailyController = require('./daily');
 
 const test = () => {
-    
+    console.log("h-page");
 }
 
 module.exports = { authController, productsController, dailyController };
