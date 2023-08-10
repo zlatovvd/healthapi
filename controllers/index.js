695bf5a56d633bd0d423d775c16080d57396081e
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-08-10 23:10:11 +0300

    page-new

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const productsController = require('./products');
 const dailyController = require('./daily');
 
 const test = () => {
-    
+    console.log('h-page-new');
 }
 
 module.exports = { authController, productsController, dailyController };
