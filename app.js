74d037a39e7fc0c9a1a48085013720ec5e86c329
zlatovvd <zlatovvd@ukr.net>
2023-08-14 22:05:57 +0300

    swagger

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const { authRouter, productsRouter, dailyRouter } = require("./routes");
-
 
 const app = express();
 
@@ -13,6 +13,9 @@ app.use(express.static("public"));
 app.use("/products", productsRouter);
 app.use("/users", authRouter);
 app.use("/daily", dailyRouter);
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({message: 'Not found'})
